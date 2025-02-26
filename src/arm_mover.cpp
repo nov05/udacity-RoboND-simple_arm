6b67a6ae8e63e3feed1bc7594499e375ce82cadc
nov05 <215590+Nov05@users.noreply.github.com>
2025-02-26 17:13:20 -0600

    .

--- src/arm_mover.cpp
+++ src/arm_mover.cpp
@@ -28,13 +28,15 @@ std::vector<float> clamp_at_boundaries(float requested_j1, float requested_j2)
     if (requested_j1 < min_j1 || requested_j1 > max_j1)
     {
         clamped_j1 = std::min(std::max(requested_j1, min_j1), max_j1);
-        ROS_WARN_STREAM(node_name << ": j1 is out of bounds, valid range (%1.2f,%1.2f), clamping to: %1.2f", min_j1, max_j1, clamped_j1);
+        ROS_WARN("%s: j1 is out of bounds, valid range (%1.2f,%1.2f), clamping to: %1.2f", 
+            node_name, min_j1, max_j1, clamped_j1);
     }
     // Check if joint 2 falls in the safe zone, otherwise clamp it
     if (requested_j2 < min_j2 || requested_j2 > max_j2)
     {
         clamped_j2 = std::min(std::max(requested_j2, min_j2), max_j2);
-        ROS_WARN_STREAM(node_name << ": j2 is out of bounds, valid range (%1.2f,%1.2f), clamping to: %1.2f", min_j2, max_j2, clamped_j2);
+        ROS_WARN("%s: j2 is out of bounds, valid range (%1.2f,%1.2f), clamping to: %1.2f", 
+            node_name.c_str(), min_j2, max_j2, clamped_j2);
     }
 
     // Store clamped joint angles in a clamped_data vector
@@ -48,7 +50,8 @@ bool handle_safe_move_request(simple_arm::GoToPosition::Request &req,
                               simple_arm::GoToPosition::Response &res)
 {
     std::string node_name = ros::this_node::getName();
-    ROS_INFO_STREAM(node_name << ": GoToPositionRequest received - j1:%1.2f, j2:%1.2f", (float)req.joint_1, (float)req.joint_2);
+    ROS_INFO(": GoToPositionRequest received - j1:%1.2f, j2:%1.2f", 
+        (float)req.joint_1, (float)req.joint_2);
 
     // Check if requested joint angles are in the safe zone, otherwise clamp them
     std::vector<float> joints_angles = clamp_at_boundaries(req.joint_1, req.joint_2);
@@ -66,7 +69,7 @@ bool handle_safe_move_request(simple_arm::GoToPosition::Request &req,
     ros::Duration(3).sleep();
 
     // Return a response message
-    res.msg_feedback = node_name + ": Joint angles set - j1: " + std::to_string(joints_angles[0]) + " , j2: " + std::to_string(joints_angles[1]);
+    res.msg_feedback = node_name + ": Joint angles set - j1: " + std::to_string(joints_angles[0]) + ", j2: " + std::to_string(joints_angles[1]);
     ROS_INFO_STREAM(res.msg_feedback);
 
     return true;
